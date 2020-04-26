2c23bde445a750fdad1615a1a7a2a48dfd970540
khaled mohamed elsayed <khaledlsayed@gmail.com>
2020-04-26 15:08:26 +0200

    IBaction and passing variables done

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by khaled mohamed elsayed on 4/26/20.
+//  Copyright © 2020 khaled mohamed elsayed. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
